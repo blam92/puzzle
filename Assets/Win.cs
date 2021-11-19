9fc5d80cdaf91a785f3f6bd862dd4a493d2010b2
Santiago Iraola <iraola.santi@gmail.com>
2021-11-19 20:16:03 -0300

    Win Message

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Win : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        Text myText = GameObject.Find("Canvas/Text").GetComponent<Text>();
+        myText.text = "You Won!!";
     }
 }
